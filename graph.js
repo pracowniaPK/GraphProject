5bde3fb830426e7994187e1e7ad12bdbb6c112cd
Leksiooon <sierocinski.maciej@o2.pl>
2018-06-16 23:53:53 +0200

    tworzenie basic_graph

--- graph.js
+++ graph.js
@@ -46,26 +46,28 @@ function zczytaj() {
     neighborMatrix[k][j] = 1;
   }
 
+  // create an array with nodes
+  var tab1 = new Array();
+  for (i = 0; i < unique.length; i++) {
+    tab1.push({ id: i, label: unique[i] });
+  }
+
+  var nodes = new vis.DataSet(tab1);
+
   console.log(unique);
   console.log(neighborMatrix);
 
-  // create an array with nodes
-  var nodes = new vis.DataSet([
-    { id: 1, label: "A" },
-    { id: 2, label: "B" },
-    { id: 3, label: "C" },
-    { id: 4, label: "D" },
-    { id: 5, label: "E" }
-  ]);
-
   // create an array with edges
-  var edges = new vis.DataSet([
-    { from: 1, to: 3, color: { color: "#ef8fd2", opacity: 0.3 } },
-    { from: 1, to: 2, color: { color: "#ef8fd2" } },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 3, to: 3 }
-  ]);
+  var tab2 = new Array();
+  for (i = 0; i < neighborMatrix.length; i++) {
+    for (j = 0; j <= i; j++) {
+      if (neighborMatrix[i][j] == 1) {
+        tab2.push({ from: i, to: j });
+      }
+    }
+  }
+
+  var edges = new vis.DataSet(tab2);
 
   // create a network
   var container = document.getElementById("basic_graph");
