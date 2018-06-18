b28ffa9073aa34fbec9cfcaff82f856c31d32893
Leksiooon <sierocinski.maciej@o2.pl>
2018-06-18 22:21:17 +0200

    Dokonczone

--- graph.js
+++ graph.js
@@ -2,13 +2,22 @@
 function zczytaj() {
   //read text form textarea
   var input = document.getElementById("input");
-  var VALUE = input.value;
+  tmpLst = getGraphMatrix(input.value);
+  unique = tmpLst[0];
+  neighborMatrix = tmpLst[1];
 
+  console.log(unique);
+  console.log(neighborMatrix);
+
+  algorytmSkojarzen(unique, neighborMatrix);
+}
+
+function getGraphMatrix(rawInput) {
   //split value to verticles
-  var tab = VALUE.split(/\n|\s/);
+  var tab = rawInput.split(/\n|\s/);
 
   //split value to edges
-  var edges = VALUE.split(/\n/);
+  var edges = rawInput.split(/\n/);
   var unique = new Array();
 
   for (i = 0; i < tab.length; i++) {
@@ -46,6 +55,55 @@ function zczytaj() {
     neighborMatrix[k][j] = 1;
   }
 
+  return [unique, neighborMatrix];
+}
+
+function algorytmSkojarzen(unique, neighborMatrix) {
+  ////////////////////////////////////////////////////////////
+  //Skojarzenie maksymalne (ang. Maximal matching)
+  ////////////////////////////////////////////////////////////
+
+  //copy unique
+
+  var table = unique.slice();
+
+  tab2 = new Array();
+  var licznik = 0;
+  while (table.length > 1) {
+    var a = table[0];
+    var b = table[1];
+
+    for (i = 0; i < unique.length; i++) {
+      if (unique[i] == a) {
+        break;
+      }
+    }
+
+    for (j = 0; j < unique.length; j++) {
+      if (unique[j] == b) break;
+    }
+
+    if (neighborMatrix[i][j] == 1) {
+      // tab2.push({ from: i, to: j, color: "#000000" });
+      neighborMatrix[i][j] = 2;
+      neighborMatrix[j][i] = 2;
+      table.shift();
+      table.shift();
+      licznik = 0;
+    } else {
+      table.shift();
+      table.push(a);
+      licznik += 1;
+    }
+    if (licznik > table.length) break;
+  }
+
+  drawGraph(unique, neighborMatrix, "graph");
+
+  return neighborMatrix;
+}
+
+function drawGraph(unique, neighborMatrix, elementId) {
   // create an array with nodes
   var tab1 = new Array();
   for (i = 0; i < unique.length; i++) {
@@ -58,73 +116,28 @@ function zczytaj() {
   for (i = 0; i < neighborMatrix.length; i++) {
     for (j = 0; j <= i; j++) {
       if (neighborMatrix[i][j] == 1) {
-        tab2.push({ from: i, to: j });
+        tab2.push({
+          from: i,
+          to: j,
+          width: 1.5,
+          color: { color: "#405dfc", opacity: 0.3 }
+        });
+      } else if (neighborMatrix[i][j] == 2) {
+        tab2.push({ from: i, to: j, width: 3, color: { color: "#fc4141" } });
       }
     }
   }
   var edges = new vis.DataSet(tab2);
 
   // create a network
-  var container = document.getElementById("basic_graph");
+  var container = document.getElementById(elementId);
   var data = {
     nodes: nodes,
     edges: edges
   };
   var options = {};
   var network = new vis.Network(container, data, options);
-
-  console.log(unique);
-  console.log(neighborMatrix);
-
-  ////////////////////////////////////////////////////////////
-  //Skojarzenie maksymalne (ang. Maximal matching)
-  ////////////////////////////////////////////////////////////
-
-  //copy unique
-
-  var table = unique.slice();
-
-  tab2 = new Array();
-  while (table.length > 1) {
-    console.log(tab2);
-    var a = table[0];
-    var b = table[1];
-
-    console.log(a);
-    console.log(b);
-    for (i = 0; i < unique.length; i++) {
-      if (unique[i] == a) {
-        break;
-      }
-    }
-
-    for (j = 0; j < unique.length; j++) {
-      if (unique[j] == b) break;
-    }
-
-    if (neighborMatrix[i][j] == 1) {
-      tab2.push({ from: i, to: j, color: "#000000" });
-      table.shift();
-      table.shift();
-      console.log("dupa");
-    } else {
-      table.shift();
-      table.push(a);
-      console.log("nie dupa");
-    }
-  }
-  var edges2 = new vis.DataSet(tab2);
-  // create a network
-  var container2 = document.getElementById("max_graph");
-  var data2 = {
-    nodes: nodes,
-    edges: edges2
-  };
-  var options = {};
-  var network = new vis.Network(container2, data2, options);
 }
-
-function getGraphMatrix(rawInput) {}
 
 /*
 { from: 1, to: 3, color: { color: "#ef8fd2", opacity: 0.3 } },
